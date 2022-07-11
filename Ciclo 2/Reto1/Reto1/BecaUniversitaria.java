6a103066ba582ebd3f2cf10c4352e25b857ebc5c
JeisonArrubla <jeison.arrubla@gmail.com>
2022-07-10 23:49:03 -0500

    Reto 1

---
+++ Ciclo 2/Reto1/Reto1/BecaUniversitaria.java
@@ -0,0 +1,44 @@
+package Reto1;
+
+public class BecaUniversitaria {
+    private int tiempo;
+    private double monto;
+    private double interes;
+    public BecaUniversitaria() {
+        tiempo = 0;
+        monto = 0;
+        interes = 0;
+    }
+    public BecaUniversitaria(int pTiempo, double pMonto, double pInteres) {
+        this.tiempo = pTiempo;
+        this.monto = pMonto;
+        this.interes = pInteres;
+    }
+    public double calcularInteresSimple() {
+        double interesSimple = monto * (interes / 100) * tiempo;
+        return Math.round(interesSimple);
+    }
+    public double calcularInteresCompuesto() {
+        double interesCompuesto = monto * (Math.pow((1 + interes / 100), tiempo) - 1);
+        return Math.round(interesCompuesto);
+    }
+    public String compararInversion(int pTiempo, double pMonto, double pInteres) {
+        this.tiempo = pTiempo;
+        this.monto = pMonto;
+        this.interes = pInteres;
+        double diferencia = calcularInteresCompuesto() - calcularInteresSimple();
+        if (diferencia != 0) {
+            return "La diferencia entre la proyección de interés compuesto e interés simple es: $" + diferencia;
+        } else {
+            return "No se obtuvo diferencia entre las proyecciones, revisar los parámetros de entrada.";
+        }
+    }
+    public String compararInversion() {
+        double diferencia = calcularInteresCompuesto() - calcularInteresSimple();
+        if (diferencia != 0) {
+            return "La diferencia entre la proyección de interés compuesto e interés simple es: $" + diferencia;
+        } else {
+            return "No se obtuvo diferencia entre las proyecciones, revisar los parámetros de entrada.";
+        }
+    }
+}
