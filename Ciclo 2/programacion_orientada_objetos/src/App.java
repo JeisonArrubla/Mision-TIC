444173f95ae66331a8ceba7ac3a65eed47c2c59a
JeisonArrubla <jeison.arrubla@gmail.com>
2022-07-11 21:37:22 -0500

    update

--- Ciclo 2/programacion_orientada_objetos/src/App.java
+++ Ciclo 2/programacion_orientada_objetos/src/App.java
@@ -1,11 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Construir un objeto de tipo Persona
-        Persona objPersona1 = new Persona("Jeison","Arrubla");
+        Persona objPersona1 = new Persona("Jeison","Arrubla",27,"1039",'M');
         //Obtener el nombre y apellido
         String nombre = objPersona1.getNombre();
         String apellido = objPersona1.getApellido();
         // Imprimir en consola
         System.out.println("Nombre: "+nombre+"\nApellido: "+apellido);
+
+        // Cambiar nombre y apellido
+        objPersona1.setNombre("JEISON");
+        objPersona1.setApellido("ARRUBLA");
+        nombre = objPersona1.getNombre();
+        apellido = objPersona1.getApellido();
+        // Imprimir en consola
+        System.out.println("Nombre: "+nombre+"\nApellido: "+apellido);
+
+        // Obtener valor nómina
+        double nomina = objPersona1.calcular_nomina(10);
+        System.out.println("Nómina: "+nomina);
+
+        // Crear más personas
+        Persona objPersona2 = new Persona("Juliana","Sandoval",20,"1152",'F');
+        objPersona2.setTelefono("3123546789");
+        objPersona2.setEmail("juliana@gmail.com");
+
+        Persona objPersona3 = new Persona("Laila","Narváez",26,"1139",'F');
+        objPersona3.setTelefono("3205667890");
+        objPersona3.setEmail("laila@gmail.com");
     }
 }
--- Ciclo 2/programacion_orientada_objetos/src/Persona.java
+++ Ciclo 2/programacion_orientada_objetos/src/Persona.java
@@ -1,47 +1,103 @@
 public class Persona {
 
     /*
-    Las clases tienen 5 secciones:
-    -Atributos
-    -Métodos constructores
-    -Métodos consultores
-    -Métodos modificadores
-    -Acciones
-    */
+     * Las clases tienen 5 secciones:
+     * -Atributos
+     * -Métodos constructores
+     * -Métodos consultores
+     * -Métodos modificadores
+     * -Acciones
+     * 
+     * No es obligatorio tenerlas todas
+     */
 
     // ATRIBUTOS
     private String nombre;
     private String apellido;
-    
+    private int edad;
+    private String cedula;
+    private String telefono;
+    private String email;
+    private char genero;
+
     // MÉTODO CONSTRUCTOR
-    public Persona(String nombre, String apellido){
+    public Persona(String nombre, String apellido, int edad, String cedula, char genero) {
+        // Pedimos por parámetro los datos obligatorios
         // this.nombre -> Atributo
         // nombre (solo) -> Parámetro
-        this.nombre = nombre; 
+        this.nombre = nombre;
         this.apellido = apellido;
         // Con this accedemos a los atributos y métodos de la clase
-        // System.out.println("Hola, soy una persona, soy "+this.nombre+" "+this.apellido);
+        // System.out.println("Hola, soy una persona, soy "+this.nombre+"
+        // "+this.apellido);
+        this.edad = edad;
+        this.cedula = cedula;
+        this.genero = genero;
     }
 
     // MÉTODOS CONSULTORES
     // Nombrar con get por estandar
-    public String getNombre(){
+    public String getNombre() {
         return nombre;
     }
 
-    public String getApellido(){
+    public String getApellido() {
         return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getTelefono(){
+        return telefono;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public char getGenero(){
+        return genero;
     }
 
     // MÉTODOS MODIFICADORES
     // Nombrar con set por estandar
-    public void setNombre(String nombre){
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public void setApellido(String apellido){
+    public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    public void setCedula(String cedula){
+        this.cedula = cedula;
+    }
+
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setGenero(char genero){
+        this.genero = genero;
+    }
+
     // ACCIONES
+    public double calcular_nomina(double valor_hora) {
+        int cant_horas = 10;
+        return (cant_horas * valor_hora);
+    }
 }
