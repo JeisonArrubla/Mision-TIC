8f168d1f923f3ed15e3afdb8629481b252f51fd8
JeisonArrubla <jeison.arrubla@gmail.com>
2022-07-16 18:23:41 -0500

    estructura clase carro

---
+++ Ciclo 2/programacion_orientada_objetos/src/Carro.java
@@ -0,0 +1,18 @@
+public class Carro {
+    // ATRIBUTOS
+    private String color;
+    private String matricula;
+    private String modelo;
+    private int cap_personas;
+    private String tipo_carro;
+    private double val_maxima;
+    private boolean gasolina;
+
+    // CONSTRUCTOR
+
+    // CONSULTORES
+
+    // MODIFICADORES
+
+    // ACCIONES
+}
