750af0eac43811fe9fe95bfb24b63dc82d7c677e
JeisonArrubla <jeison.arrubla@gmail.com>
2022-07-17 23:12:31 -0500

    sobrecarga de métodos

---
+++ Ciclo 2/empresa/src/main/java/com/empresa/Empleado.java
@@ -0,0 +1,60 @@
+package com.empresa;
+
+public class Empleado {
+    // ATRIBUTOS
+    private String nombre;
+    private int edad;
+    private double sueldoBruto;
+
+    // CONSTRUCTORES
+    // SOBRECARGA DE MÉTODOS
+    // Sobre carga de métodos -> Crear varios métodos con el mismo nombre pero con
+    // comportamientos distintos
+    public Empleado(String nombre, int edad, double sueldoBruto) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sueldoBruto = sueldoBruto;
+    }
+
+    public Empleado(String nombre, int edad) {
+        // Se llama igual pero con parámetros distintos
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Empleado() {
+        // Se llama igual pero sin parámetros
+        this.nombre = "";
+        this.edad = 0;
+        this.sueldoBruto = 0.0;
+    }
+
+    // CONSULTORES - GETTERS
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public double getSueldoBruto() {
+        return sueldoBruto;
+    }
+
+    // MODIFICADORES - SETTERS
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSueldoBruto(double sueldoBruto) {
+        this.sueldoBruto = sueldoBruto;
+    }
+
+}
