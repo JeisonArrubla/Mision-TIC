f5580e910228f65e4c80bd47502686b17ed0453e
JeisonArrubla <jeison.arrubla@gmail.com>
2022-07-17 22:50:08 -0500

    reto 2

---
+++ Ciclo 2/Reto2/reto2/src/PrecioTotal.java
@@ -0,0 +1,32 @@
+public class PrecioTotal {
+    private Double totalComputadores = 0.0;
+    private Double totalComputadoresPortatiles = 0.0;
+    private Double totalComputadoresMesa = 0.0;
+    private Computadores[] listaComputadores;
+
+    // Constructor
+    PrecioTotal(Computadores[] pComputadores) {
+        this.listaComputadores = pComputadores;
+    }
+
+    public void mostrarTotales() {
+        ComputadoresPortatiles objP = new ComputadoresPortatiles();
+        ComputadoresMesa objM = new ComputadoresMesa();
+
+        for(int i = 0; i < listaComputadores.length; i++){
+            Computadores computador = listaComputadores[i];
+            if(computador.getClass() == objP.getClass()){
+                totalComputadoresPortatiles += computador.calcularPrecio();
+            }else if(computador.getClass() == objM.getClass()){
+                totalComputadoresMesa += computador.calcularPrecio();
+            }
+            totalComputadores += computador.calcularPrecio();
+            
+        }
+        
+        // Mostramos los resultados
+        System.out.println("La suma del precio de los computadores es de " + totalComputadores);
+        System.out.println("La suma del precio de los computadores de mesa es de " + totalComputadoresMesa);
+        System.out.print("La suma del precio de los computadores portátiles es de " + totalComputadoresPortatiles);
+    }
+}
