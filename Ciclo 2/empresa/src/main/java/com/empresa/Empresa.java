17060619bdd228fc80c3a9fdd106ebb4365e2485
JeisonArrubla <jeison.arrubla@gmail.com>
2022-07-17 23:49:18 -0500

    update

--- Ciclo 2/empresa/src/main/java/com/empresa/Empresa.java
+++ Ciclo 2/empresa/src/main/java/com/empresa/Empresa.java
@@ -1,27 +1,34 @@
 package com.empresa;
 
+import java.util.Scanner;
+
 public class Empresa {
     // ATRIBUTOS
     private String nombre;
     private Empleado empleado; // Relación fuerte entre empresa y empleado
 
-    // CONSTRUCTOR 
+    // CONSTRUCTOR
     // SOBRECARGA DE MÉTODOS
-    public Empresa(String nombre){
+    public Empresa(String nombre) {
         this.nombre = nombre;
+
+        crearEmpleado();
+        // Al momento de crear una empresa vamos a crear un empleado, por eso el método se llama desde el constructor 
     }
 
-    public Empresa(){
+    public Empresa() {
         this.nombre = "";
+
+        crearEmpleado();
     }
 
     // CONSULTORES - GETTERS
 
-    public String getNombre(){
+    public String getNombre() {
         return nombre;
     }
 
-    public Empleado getEmpleado(){
+    public Empleado getEmpleado() {
         return empleado;
     }
 
@@ -35,10 +42,40 @@ public class Empresa {
         this.empleado = empleado;
     }
 
-    // ACCIONES 
-    public Cliente registrarCliente(){
-        // 
-        return ;
+    // ACCIONES
+    public void crearEmpleado() {
+        try (Scanner leer = new Scanner(System.in)) {
+            System.out.print("Por favor ingrese el nombre del empleado: ");
+            String nombre = leer.next();
+
+            System.out.print("Por favor ingrese la edad del emplead0: ");
+            int edad = leer.nextInt();
+
+            empleado = new Empleado(nombre, edad);
+
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
     }
-    
+
+    public Cliente registrarCliente() {
+        Cliente cliente = new Cliente();
+
+        try(Scanner leer = new Scanner(System.in)){
+
+            System.out.print("Ingrese el nombre del cliente: ");
+            String nombre = leer.next();
+
+            System.out.print("Ingrese el apellido del cliente: ");
+            String apellido = leer.next();
+
+            cliente = new Cliente(nombre, apellido);
+
+        }catch(Exception error){
+            System.out.println(error.getMessage());
+        }
+
+        return cliente;
+    }
+
 }
