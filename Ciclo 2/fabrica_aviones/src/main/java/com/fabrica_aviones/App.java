9a0f0f0a96018eaffc66819138d1403bfce2d050
JeisonArrubla <jeison.arrubla@gmail.com>
2022-07-18 23:37:52 -0500

    primer commit

---
+++ Ciclo 2/fabrica_aviones/src/main/java/com/fabrica_aviones/App.java
@@ -0,0 +1,13 @@
+package com.fabrica_aviones;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
