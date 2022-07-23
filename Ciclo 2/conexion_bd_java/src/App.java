f9a7733899756753c822415219be4f3df3e7105f
JeisonArrubla <jeison.arrubla@gmail.com>
2022-07-23 11:32:17 -0500

    cobjeto connection

--- Ciclo 2/conexion_bd_java/src/App.java
+++ Ciclo 2/conexion_bd_java/src/App.java
@@ -1,5 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        try {
+
+            // Objeto de conexión
+            // Con este objeto podremos ejecutar instrucciones de tipo SQL
+            // Me va retornar un objeto de tipo result set
+            // Result set es una tabla que nos va mostrar el resultado, lo vamos a iterar
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:hr.db");
+            if (conn != null) {
+                System.out.println("Conexión exitosa a la base de datos");
+            }
+
+        } catch (SQLException error) {
+            System.out.println(error.getMessage());
+        }
+
     }
 }
