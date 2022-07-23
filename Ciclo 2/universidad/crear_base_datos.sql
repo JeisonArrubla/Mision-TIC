d49586e827ac3d6f0ce6502f7caf374436f6f781
JeisonArrubla <jeison.arrubla@gmail.com>
2022-07-23 18:06:27 -0500

    creación bd

---
+++ Ciclo 2/universidad/crear_base_datos.sql
@@ -0,0 +1,51 @@
+--CREAR TABLA UNIVERSIDAD
+CREATE TABLE Universidades(
+	nit VARCHAR(20) PRIMARY KEY NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+	direccion VARCHAR(100) NOT NULL,
+	email VARCHAR(120) NOT NULL
+);
+
+--El que lleva la cardinalidad de muchos va a llevar el id de la otra tabla (llave foránea)
+
+--CREAR TABLA TelefonosUniversidad
+CREATE TABLE TelefonosUniversidad (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	numero_telefono VARCHAR(15) NOT NULL,
+	universidad_nit VARCHAR(20) NOT NULL,
+	FOREIGN KEY(universidad_nit) 
+		REFERENCES Universidades(nit)
+);
+
+
+--CREAR TABLA FACULTADES
+CREATE TABLE Facultades(
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	codigo VARCHAR(10) NOT NULL,
+	nombre VARCHAR(50) NOT NULL,
+	universidad_nit VARCHAR(20) NOT NULL,
+	FOREIGN KEY(universidad_nit) 
+		REFERENCES Universidades(nit)
+);
+
+--CREAR TABLA Estudiantes
+CREATE TABLE Estudiantes(
+	cedula VARCHAR(15) PRIMARY KEY NOT NULL,
+	codigo VARCHAR(10) NOT NULL,
+	nombre VARCHAR(60) NOT NULL,
+	apellido VARCHAR(60) NOT NULL,
+	edad INTEGER NOT NULL,
+	sexo CHAR NOT NULL,
+	facultad_id INTEGER NOT NULL,
+	FOREIGN KEY(facultad_id)
+		REFERENCES Facultades(id)
+);
+
+--CREAR TABLA TelefonosEstudiantes
+CREATE TABLE TelefonosEstudiantes (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	numero_telefono VARCHAR(15) NOT NULL,
+	estudiante_cedula VARCHAR(15) NOT NULL,
+	FOREIGN KEY(estudiante_cedula)
+		REFERENCES Estudiantes(cedula)
+);
